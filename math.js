276040c87428ebf92b7f7421ade2e2f8eff010a0
graceshotts15 <78330999+graceshotts15@users.noreply.github.com>
2021-10-11 04:53:38 -0500

    Add files via upload

---
+++ math.js
@@ -0,0 +1,11 @@
+
+    let num1 = Math.floor(Math.random()*6)+1;
+    let num2 = Math.floor(Math.random()*6)+1;
+    let num3 = Math.floor(Math.random()*6)+1;
+    let num4 = Math.floor(Math.random()*6)+1;
+    let num5 = Math.floor(Math.random()*6)+1;
+
+
+
+
+
